7b3c61c6f2ed664d022208784b271f839e9bf94b
ronak2 <ronakt222@gmail.com>
2021-12-05 19:42:31 -0600

    Added variable chart views

--- crypto_simulator/routes/dashboard.js
+++ crypto_simulator/routes/dashboard.js
@@ -14,44 +14,24 @@ router.get('/', function(req, res, next) {
     console.log(req.user);
 
     current_id = req.user.email;
-    console.log("Here: %s",current_id);
 
-    // new Chart(document.getElementById("bar-chart"), {
-    //     type: 'bar',
-    //     data: {
-    //       labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
-    //       datasets: [
-    //         {
-    //           label: "Population (millions)",
-    //           backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
-    //           data: [2478,5267,734,784,433]
-    //         }
-    //       ]
-    //     },
-    //     options: {
-    //       legend: { display: false },
-    //       title: {
-    //         display: true,
-    //         text: 'Trading volumes'
-    //       }
-    //     }
-    // });
     
     axios.get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=bitcoin%2C%20ethereum%2C%20cardano%2C%20ripple%2C%20solana&order=market_cap_desc&per_page=100&page=1&sparkline=false").then(function(response){
         let btcp = response.data[0].current_price;
         let btcm = response.data[0].market_cap;
+        let ethp = response.data[1].current_price;
         let ethm = response.data[1].market_cap;
+        let solp = response.data[2].current_price;
         let solm = response.data[2].market_cap;
+        let adap = response.data[3].current_price;
         let adam = response.data[3].market_cap;
+        let xrpp = response.data[4].current_price;
         let xrpm = response.data[4].market_cap;
-        console.log(adam);
-        res.render('dashboard', {userInfo, btcp, btcm, ethm, solm, adam, xrpm});
+        
+        res.render('dashboard', {userInfo, btcp, btcm, ethp, ethm, solp, solm, adap, adam, xrpp, xrpm});
     }).catch(function(error){
         console.log(error);
     })
-
-
-
     
 });
 
@@ -60,8 +40,6 @@ router.get('/updateAccount', (req, res) => res.render('updateAccount'));
 
 //Get the delete account page
 router.get('/deleteAccount', (req, res) => res.render('deleteAccount'));
-
-
 
 //Handle the account updating
 router.post('/updateAccount', async (req, res, next) => {
